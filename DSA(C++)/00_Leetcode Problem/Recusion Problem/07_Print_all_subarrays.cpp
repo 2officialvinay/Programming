b2605e6cfaafdbb516d6c7a9b7996cf9413416cc
Vinay Saini <2officialvinay@gmail.com>
2026-02-09 23:42:20 +0530

    printing Subarrays

--- DSA(C++)/00_Leetcode Problem/Recusion Problem/07_Print_all_subarrays.cpp
+++ DSA(C++)/00_Leetcode Problem/Recusion Problem/07_Print_all_subarrays.cpp
@@ -1,25 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void print(vector<int> &arr){
+void print2(vector<int> &arr, int start, int end){
     // Base Case...
+    if(end == arr.size()){
+        return;
+    }
 
     // Progression...
-    for(int i = 0; i < arr.size() - 1; i++){
-        for(int j = 0; j < i; j++){
-            cout << arr[j] << " ";
-        }
+    for(int i = start; i <= end; i++){
+        cout << arr[i] << " ";
     }
+    cout << endl;
     
     // Recursive Relation...
-    print(arr)
+    print2(arr, start, end + 1);
+}
 
+void print1(vector<int> &arr, int start, int end){
+   for(int i = 0; i < arr.size(); i++){
+        print2(arr, i, i);
+   }
 }
 
 int main(){
     vector<int>arr{1,2,3,4,5};
 
-    print(arr);
+    print1(arr, 0, 0);
 
     return 0;
 }
