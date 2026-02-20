9deef91cee6cc91b229bb4a3749b1107892a07e4
Vinay Saini <2officialvinay@gmail.com>
2026-02-21 00:24:43 +0530

    Back-Tracking

---
+++ DSA(C++)/14 Recursion/DnC/03_Backtracing_in_permutation.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printPermutation(string str, int i){
+    // Base Case...
+    if(i >= str.length()){
+        cout << str << "  ";
+        return;
+    }
+
+    for(int j = i; j < str.length(); j++){
+        // Swapping..
+        swap(str[i], str[j]);
+
+        // Recursive Call..
+        printPermutation(str, i+1);
+
+        // Back-Tracking..
+        swap(str[i], str[j]);
+    }
+}
+
+int main(){
+    string str = "abc";
+    int i = 0;
+
+    printPermutation(str, i);
+
+    return 0;
+}
