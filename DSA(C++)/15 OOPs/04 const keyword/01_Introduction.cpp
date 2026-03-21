8997b7eb5423aabfa6cb4a614237c645178d8d57
Vinay Saini <2officialvinay@gmail.com>
2026-03-21 23:21:29 +0530

    Introduction to const

---
+++ DSA(C++)/15 OOPs/04 const keyword/01_Introduction.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    const int x = 10; // x will be constant throughout the program.
+    // x = 10, x is a Read-Only varible so we can't change it.
+    cout << x << endl;
+
+    return 0;
+}
