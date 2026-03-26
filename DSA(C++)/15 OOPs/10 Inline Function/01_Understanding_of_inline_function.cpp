3958f2b2362f7f321c19cef26b09e24c21ce01d9
Vinay Saini <2officialvinay@gmail.com>
2026-03-26 23:32:30 +0530

    inline understanding

---
+++ DSA(C++)/15 OOPs/10 Inline Function/01_Understanding_of_inline_function.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+inline void numberShow(int num){
+    cout << num << endl;
+}
+
+int main(){
+    numberShow(45); // function is replaced in place of it.
+    numberShow(45);
+    numberShow(45);
+    numberShow(45);
+    numberShow(45);
+    numberShow(45);
+    numberShow(45);
+    numberShow(45);
+    numberShow(45);
+    numberShow(45);
+    numberShow(45);
+
+    return 0;
+}
