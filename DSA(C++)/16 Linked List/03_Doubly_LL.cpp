82f4e8d8bc4a4e1ea84e624c89faea3868186cb2
Vinay Saini <2officialvinay@gmail.com>
2026-03-31 23:50:05 +0530

    Understanding of Doubly LL

---
+++ DSA(C++)/16 Linked List/03_Doubly_LL.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* prev;
+    Node* next;
+
+    // Default Constructor...
+    Node() {
+        this->data = 0;
+        this->prev = NULL; 
+        this->next = NULL;
+    }
+
+    // Parameterized constructor...
+    Node(int data) {
+        this->data = data; 
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int getLength(Node* &head){
+    int len = 0;
+    Node* temp = head;
+    while(temp != 0){
+        temp = temp->next;
+        len++;
+    }
+    return len;
+}
+
+int main(){
+    Node* first = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+
+    first->next = second;
+    second->prev =  first;
+
+    second->next = third;
+    third->prev = second;
+
+    print(first);
+
+    return 0;
+}
