6ceebd4e935d2fd718ef789559196b0415595569
Vinay Saini <2officialvinay@gmail.com>
2026-04-08 21:51:30 +0530

    Finding middle node using the tortise(slow & fast)

---
+++ DSA(C++)/16 Linked List/09_Middle_node_using_slow_and_fast.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* MiddleNode(Node* &head){
+    if(head == NULL){
+        cout << "LL is empty." << endl;
+    }
+    if(head->next == NULL){
+        return head;
+    }
+
+    // More than one node...
+    Node* slow = head;
+    Node* fast = head->next;
+    while(slow != NULL && fast != NULL){
+        fast = fast->next;
+        if(fast != NULL){
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+    return slow;
+}
+
+int main(){
+    Node* head = new Node(10);
+    Node* first = new Node(20);
+    Node* second = new Node(30);
+    Node* third = new Node(40);
+    Node* fourth = new Node(50);
+    Node* fifth = new Node(60);
+    Node* sixth = new Node(60);
+
+    head->next = first;
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+
+    print(head);
+
+    cout << "Middle node is: " << MiddleNode(head)->data << endl;
+
+    return 0;
+}
