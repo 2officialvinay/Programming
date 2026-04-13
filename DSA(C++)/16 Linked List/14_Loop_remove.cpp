f04e138c424f0fa7dedce2c77c576132e7e6c949
Vinay Saini <2officialvinay@gmail.com>
2026-04-14 01:14:47 +0530

    Loop remove

---
+++ DSA(C++)/16 Linked List/14_Loop_remove.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+// Floyd's Cycle Detection...
+void LoopRemove(Node* &head){
+    if(head == NULL){
+        return;
+    }
+
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(fast == slow){
+            break;
+        }
+    }
+    slow = head;
+    Node* prev = fast;
+    while(slow != fast){
+        prev = fast;
+        slow = slow->next;
+        fast = fast->next;
+    }
+    prev->next = NULL;
+}
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main(){
+    Node* first = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+    Node* fourth = new Node(40);
+    Node* fifth = new Node(50);
+    Node* sixth = new Node(60);
+    Node* seventh = new Node(70);
+    Node* eighth = new Node(80);
+    Node* nineth = new Node(90);
+    Node* tenth = new Node(100);
+    
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = seventh;
+    seventh->next = eighth;
+    eighth->next = nineth;
+    nineth->next = tenth;
+    tenth->next = fifth;
+
+    LoopRemove(first);
+    cout << "LL after loop remove:" << endl;
+    print(first);
+
+    return 0;
+}
