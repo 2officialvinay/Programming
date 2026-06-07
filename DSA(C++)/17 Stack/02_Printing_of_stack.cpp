49f9c3d0183af0d4efa740939306fee113b7f562
Vinay Saini <2officialvinay@gmail.com>
2026-06-08 00:20:11 +0530

    Stack Printing

---
+++ DSA(C++)/17 Stack/02_Printing_of_stack.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack<int> s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+    s.push(60);
+    s.push(70);
+
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    return 0;
+}
